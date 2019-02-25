df094a3e5400698f57c38c5c8597e528ec0d8c17
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-02-25 17:32:57 -0500

    Add files via upload

---
+++ phpphp_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<h1> <center>Operaciones Matematicas Basicas</></h1>
+<body>
+
+<?php
+$num1 = 41;
+$num2 = 2.1;
+
+echo "El resultado de sumar 41 y 2.1 es: "; echo $num1 + $num2 . "<br />";
+echo "El resultado de restar 41 y 2.1 es: "; echo $num1 - $num2 . "<br />";
+echo "El resultado de dividir 41 y 2.1 es: "; echo $num1 / $num2 . "<br />";
+echo "El resultado de multiplicar 41 y 2.1 es: "; echo $num1 * $num2;
+
+?>
+
+</body>
+</html>
