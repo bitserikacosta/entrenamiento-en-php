af9cbbf3ea509b62b624a4e17c86d9aa41c42520
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-02-27 10:39:15 -0500

    Update php_4.php

--- php/php_4.php
+++ php/php_4.php
@@ -10,8 +10,9 @@
 //Se inicializa el contador
              $i=0;
 			 //Creo la variable "terminar"
-			 $terminar = 35;
-			 //establezco el valor limite para impares menores de 1000
+			 $terminar = 356;
+	
+			 		 //bucle while que muestra por pantalla todos los números impares menores de 1000. 
              while($i <= 1000){
 				 //Creo la terminacion del ciclo una vez la lectura de impares supere el de la variable $terminar
 				     if ($i > $terminar  ) {
@@ -21,7 +22,8 @@
                  if($i % 2 == 0){
                    
                  }else{
-					  //Aca se invoca la salida por pantalla de la lista de impares y tambien elimino la coma al final.
+					  //Aca se invoca la salida por pantalla de la lista de impares, ademas Los números se 
+			 //mostrarán separados por comas y  elimino la coma al final.
 
 					echo  "," . rtrim($i,"");
 					
