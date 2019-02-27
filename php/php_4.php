15be44ff9b5c5295280ec9df50a03a3fdb81360a
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-02-27 10:19:51 -0500

    Add files via upload

---
+++ php/php_4.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<h1> <center>Numeros Impares</></h1>
+<body>
+
+<?php
+
+//Se inicializa el contador
+             $i=0;
+			 //Creo la variable "terminar"
+			 $terminar = 356;
+			 //establezco el valor limite para impares menores de 1000
+             while($i <= 1000){
+				 //Creo la terminacion del ciclo una vez la lectura de impares supere el de la variable $terminar
+				     if ($i > $terminar  ) {
+        break;
+    }
+				 //Aca uso el operador %.
+                 if($i % 2 == 0){
+                   
+                 }else{
+					  //Aca se invoca la salida por pantalla de la lista de impares y tambien elimino la coma al final.
+
+					echo  "," . rtrim($i,"");
+					
+                 }
+                 $i++ ;	 	 
+             }
+   ?>
+
+</body>
+</html>
