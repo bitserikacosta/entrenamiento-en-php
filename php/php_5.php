0784e90827f567f08c23de6d112e31e87cbeebf2
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-02-28 11:40:57 -0500

    Update php_5.php

--- php/php_5.php
+++ php/php_5.php
@@ -1,31 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
+ 
 <meta charset="utf-8">
-<h1> <center>Ejercicio 5</></h1>
+<h1> <center>Ejercicio °5</></h1>
+ 
 <body>
-
+ 
 <?php 
-
-    //Definicion de 2 variables
-$cadena1 = "abcdefg"; 
-$cadena2 = "abcdefg";  
-
-    //Se procede a conocer el numero de caracteres mediante la identificacion de su longitud con strlen
+    //Definicion de las 2 variables
+ 
+$cadena1 = "abcdefghi";
+$cadena2 = "abcdefghi";  
+ 
+  //Se procede a conocer el numero de caracteres mediante la identificacion de su longitud con strlen
     //se Comparan las cadenas si son iguales, o cual es mayor con uso de las clausulas if
-   
+
 if   ($cadena1 == $cadena2)
-{
-    echo "Las cadenas son iguales tienen "; echo strlen($cadena1); echo " y "; echo strlen($cadena2); echo " caracteres";
+ 
+{ 
+   $MismaCantidad = strlen ($cadena1) + strlen($cadena2);
+echo "Las cadenas son iguales y tienen " .  $MismaCantidad . " caracteres";
 
 } elseif ($cadena1 > $cadena2) {
-    echo "La cadena1 es mayor que la cadena2 en "; echo strlen($cadena1); echo " caracteres";
+ 
+$Cadena1Superior = strlen ($cadena1) - strlen($cadena2);
+echo "la cadena1 es mayor que la cadena2 en " .  $Cadena1Superior . " caracteres";
 
 } elseif ($cadena1 < $cadena2) {
-    echo "La cadena1 es menor que la cadena2 en "; echo strlen($cadena2); echo " caracteres";	 	
+	$Cadena2Superior = strlen ($cadena2) - strlen($cadena1);
+echo "la cadena1 es menor que la cadena2 en " .  $Cadena2Superior . " caracteres";
 }
-
+ 
 ?> 
-
+ 
 </body>
 </html>
