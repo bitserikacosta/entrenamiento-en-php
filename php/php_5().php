a28395cfeb94e15d842805e4db6403e7d9346c96
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-02-28 09:51:57 -0500

    Update php_5().php

--- php/php_5().php
+++ php/php_5().php
@@ -7,12 +7,16 @@
 
 <?php 
 
+  //Definicion de 2 variables
 $cadena1 = 123; 
 $cadena2 = 127;
   
+  //Hallar la diferencia en caracteres
 $Diferencia = ($cadena1 - $cadena2);
 $Diferencia2 = ($cadena2 - $cadena1);
-
+   
+//Comparacion entre las cadenas si son iguales, o cual es mayor
+  // uso de las clausulas if
 if   ($cadena1 == $cadena2)
 {
     echo "Las cadenas son iguales tienen "; echo $cadena1; echo " y "; echo $cadena2; echo " caracteres";
