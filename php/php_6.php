26072ea7715dd5d2311fc47d0e0eced9d0f51756
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-03-01 13:11:10 -0500

    Add files via upload

---
+++ php/php_6.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+<?php
+        $employees = [
+    13 => [
+        'firstname' => 'Carolina',
+        'lastname' => 'Poveda',
+        'salary' => 900
+    ],
+    3 => [
+        'firstname' => 'Sebastian',
+        'lastname' => 'Sanchez',
+        'salary' => 2700
+    ],
+    42 => [
+        'firstname' => 'Saul',
+        'lastname' => 'Goodman',
+        'salary' => 1800
+    ]
+];
+?>
+<ul>
+<?php
+ $IdCounter = 0;
+$id = array_keys($employees);
+$EmployeeSalary = 0;
+    
+    
+ foreach ($employees as $EmployeeName) {
+ switch ($EmployeeName) {
+                case $EmployeeName['salary'] < 1000;
+     echo "<ul>"; echo "<li>";
+echo  $EmployeeName['firstname'] . " " . $EmployeeName['lastname']  .
+                        " (ID: $id[$IdCounter])  tiene un salario de: " . "$" . $EmployeeName['salary'] . " (bajo)";
+echo "</ul>";echo "</li>";
+                    break;
+         
+                          case $EmployeeName['salary'] >= 2000;
+        echo "<ul>"; echo "<li>";
+echo  $EmployeeName['firstname'] . " " . $EmployeeName['lastname']  .
+                        " (ID: $id[$IdCounter])  tiene un salario de: " . "$" . $EmployeeName['salary'] . " (alto)";
+    echo "</ul>";echo "</li>";
+                    break;
+         
+                  case $EmployeeName['salary'] >= 1000;
+    echo "<ul>"; echo "<li>";
+echo  $EmployeeName['firstname'] . " " . $EmployeeName['lastname']  .
+                        " (ID: $id[$IdCounter])  tiene un salario de: " . "$" . $EmployeeName['salary'] . " (medio)";
+    echo "</ul>";echo "</li>";
+                    break;
+ }
+ $IdCounter++;
+            if ($EmployeeSalary < $EmployeeName['salary']) {
+                $EmployeeSalary = $EmployeeName['salary'];
+                $HighestIncome = $EmployeeName['firstname'] . " " . $EmployeeName['lastname'];
+                      
+ }
+ }
+      
+          echo "<br>"; echo "<ul>"; echo "<li>"; echo "<strong>"; echo $HighestIncome . " tiene el mayor salario de todos";  echo "</strong>";
+        
+        ?>
+
+</body>
+</html>
