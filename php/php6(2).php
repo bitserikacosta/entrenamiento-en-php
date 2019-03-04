fafd66a28737cff139e4129ad86674964d438bf1
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-03-04 16:02:05 -0500

    Update php6(2).php

--- php/php6(2).php
+++ php/php6(2).php
@@ -1,17 +1,4 @@
-   <form method="post" action="index.php">           
-            <input type="number" name="num1" class="Valor">
-            <input type="number" name="num2" class="Valor">
-            <select class="opt" name="simbolo">
-                <option value="suma">+</option>
-                <option value="resta">-</option>
-                <option value="multiplicacion">x</option>
-                <option value="dividir">/</option>
-            </select>
-            <input type="submit" name="SimboloOpcion" value="valor" class="enviar">                                         
-        </form>
-
-
-		<?php 
+    <?php 
     if(isset($_POST['SimboloOpcion'])){
         $num1 = $_POST['num1'];
         $num2 = $_POST['num2'];
@@ -38,7 +25,21 @@
     }
     ?>
 	
-	
+
+        <form method="post" action="index.php">           
+            <input type="number" name="num1" class="Valor">
+            <input type="number" name="num2" class="Valor">
+            <select class="opt" name="simbolo">
+                <option value="suma">+</option>
+                <option value="resta">-</option>
+                <option value="multiplicacion">x</option>
+                <option value="dividir">/</option>
+            </select>
+            <input type="submit" name="SimboloOpcion" value="valor" class="enviar">                                         
+        </form>
+		
+		
+		
         <?php if(isset($_POST['SimboloOpcion'])){ ?>
             <input type="text" value="<?php echo $resultado; ?>" class="Valor">
         <?php }else{ ?>
